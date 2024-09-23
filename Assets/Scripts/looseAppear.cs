df4ec6b81587cb82dba427d8ee3e1972d9cf4d49
hawk <nahuel36gg@gmail.com>
2024-09-23 19:38:18 -0300

    sonido de perder el juego

--- Assets/Scripts/looseAppear.cs
+++ Assets/Scripts/looseAppear.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class looseAppear : MonoBehaviour {
 
+    [SerializeField] PlaySound looseSound;
+    [SerializeField] GameObject gameMusic;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -15,8 +18,12 @@ public class looseAppear : MonoBehaviour {
         if(this != null && this.transform != null)
         { 
 
-        Transform child = this.transform.GetChild(0);
-        child.gameObject.SetActive(true);
+            Transform child = this.transform.GetChild(0);
+            child.gameObject.SetActive(true);
+
+            looseSound.Play();
+            gameMusic.GetComponentInParent<Animator>().enabled = false;
+            gameMusic.SetActive(false);
         }
     }
 }
