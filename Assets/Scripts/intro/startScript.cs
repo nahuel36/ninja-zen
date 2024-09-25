dbb50d6c4d235f0149bfcb6f4c53363db29634d6
hawk <nahuel36gg@gmail.com>
2024-09-24 21:34:25 -0300

    que se pueda pasar el intro con el boton de start

--- Assets/Scripts/intro/startScript.cs
+++ Assets/Scripts/intro/startScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class startScript : MonoBehaviour
 {
-
-/*
+    InputAction accept;
+    [SerializeField] float splashTime = 3;
+    [SerializeField] Animator menuAnimator;
+    bool pressedstart = false;
     public void Start()
     {
-       // Resources.LoadAll("backgrounds");
+        pressedstart = false;
+        accept = InputSystem.actions.FindAction("Accept");
+        // Resources.LoadAll("backgrounds");
     }
-    */
+
+    public void Update()
+    {
+        if (Time.timeSinceLevelLoad < splashTime || pressedstart) return;
+
+        if (accept.WasPressedThisFrame())
+        { 
+            pressedstart = true;
+            menuAnimator.SetTrigger("start");
+        }
+    }
+
     public void pressedStart()
     {
         SceneManager.LoadScene("game");
